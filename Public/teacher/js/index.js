e01b0cd8b4da8c8a2d66321e55218fb3d4261809
452757438@qq.com <452757438@qq.com>
2018-09-30 17:32:30 +0800

    s

--- Public/teacher/js/index.js
+++ Public/teacher/js/index.js
@@ -3,12 +3,13 @@ $(function() {
         el: '#teacher',
         data: {
             academic: [
+            // {
+            //     img: "http://vip.xiaoying.net/img/teacher/small/cyy.jpg",
+            //     name: "陈远英",
+            //     job: "精英首席学术研究顾问",
+            //     desc: "东京大学金融工学。师从野村证券首席战略分析师新井富雄老师。8年私塾讲师经手案例近万人，擅长TOP10顶尖名校升学。"
+            // }, 
             {
-                img: "http://vip.xiaoying.net/img/teacher/small/cyy.jpg",
-                name: "陈远英",
-                job: "精英首席学术研究顾问",
-                desc: "东京大学金融工学。师从野村证券首席战略分析师新井富雄老师。8年私塾讲师经手案例近万人，擅长TOP10顶尖名校升学。"
-            }, {
                 img: "http://vip.xiaoying.net/img/teacher/small/jym.jpg",
                 name: "简依梦",
                 job: "精英计划高级学术顾问",
@@ -23,12 +24,14 @@ $(function() {
                 name: "尚海秋",
                 job: "精英首席签证材料顾问",
                 desc: "28年的日本文化与习惯的交流经验，作为20年赴日日本留学工作资深业内人士，深谙日本各大使馆及入管局对留学签证材料的要诣，有过多批签证率百分百的记录，还有多次拒签再获签的成功经验，特别擅长对于疑难复杂材料的修整和完善，近20年来得到无数赴日留学生和家长的安心委托与感谢！也赢得日本校方的高度信任与长期合作……"
-            }, {
-                img: "http://vip.xiaoying.net/img/teacher/small/yszj.jpg",
-                name: "远山智久",
-                job: "精英计划高级学术顾问",
-                desc: "东京大学博士。20年以上教学经验，擅长东京大学京都大学等顶级名校升学指导，指导学员东京大学录取率常年保持在50%以上。"
-            },{
+            }, 
+            // {
+            //     img: "http://vip.xiaoying.net/img/teacher/small/yszj.jpg",
+            //     name: "远山智久",
+            //     job: "精英计划高级学术顾问",
+            //     desc: "东京大学博士。20年以上教学经验，擅长东京大学京都大学等顶级名校升学指导，指导学员东京大学录取率常年保持在50%以上。"
+            // },
+            {
                 img: "http://vip.xiaoying.net/img/teacher/small/ywy.jpg",
                 name: "杨文颖",
                 job: "精英计划高级学术顾问",
