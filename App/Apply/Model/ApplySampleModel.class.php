944595d1735af75bcdbb86b58e439a2e3a03169a
dragon <jialongfeicn@gmail.com>
2018-10-23 16:48:25 +0800

    修改我的申请数据接口

---
+++ App/Apply/Model/ApplySampleModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dragon
+ * Date: 2018/10/23
+ * Time: 15:47
+ */
+
+namespace Apply\Model;
+
+class ApplySampleModel extends CRMBaseModel
+{
+    protected $tableName = 'apply_materials';
+}
